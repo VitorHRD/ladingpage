7fcdad553a7cc7f4c7822f21d48ae534b5df3b81
VitorHRD <vrdev435@gmail.com>
2022-02-04 17:45:09 -0300

    origin

--- js/Darktheme.js
+++ js/Darktheme.js
@@ -12,83 +12,102 @@ let line1 = document.getElementById("line1");
 let line2 = document.getElementById("line2");
 let line3 = document.getElementById("line3");
 let botao = document.getElementById("botao");
+let buttons = document.querySelectorAll(".buttongroup")
 
 function emojicon() {
 
-    if (emoji.style.display=="block") {
-        
+    if (emoji.style.display == "block") {
+
         emoji.style.display = "none";
         emoji2.style.display = "block";
-        img.src="./imagens/logo.png";
+        img.src = "./imagens/logo.png";
         colorBlack();
         backGroundWhite();
-        localStorage.setItem("theme","light");
+        buttonsRed();
+        localStorage.setItem("theme", "light");
 
     }
     else {
         emoji.style.display = "block";
         emoji2.style.display = "none";
-        img.src="./imagens/logoblack.png";
+        img.src = "./imagens/logoblack.png";
         colorWhite();
         backGroundBlack();
-        localStorage.setItem("theme","dark");
+        buttonsYellow();
+        localStorage.setItem("theme", "dark");
 
     }
 }
 window.onload = checkTheme();
-function checkTheme(){
+function checkTheme() {
     let localStorageTheme = localStorage.getItem("theme");
-    if(localStorageTheme != null && localStorageTheme === "light"){
+    if (localStorageTheme != null && localStorageTheme === "light") {
         emoji.style.display = "none";
         emoji2.style.display = "block";
         colorBlack();
         backGroundWhite();
+        buttonsRed();
     }
-    else{
+    else {
         emoji2.style.display = "none";
         emoji.style.display = "block";
-        img.src="./imagens/logoblack.png"
+        img.src = "./imagens/logoblack.png"
         colorWhite();
         backGroundBlack();
+        buttonsYellow();
     }
 }
-function colorWhite(){
-    dark.style.color="white";
-    dark1.style.color="white";
-    dark2.style.color="white";
-    dark3.style.color="white";
-    h2.style.color ="white";
-    h1.style.color ="#ffc107";
+function colorWhite() {
+    dark.style.color = "white";
+    dark1.style.color = "white";
+    dark2.style.color = "white";
+    dark3.style.color = "white";
+    h2.style.color = "white";
+    h1.style.color = "#ffc107";
 }
-function colorBlack(){
-    dark.style.color="black";
-    dark1.style.color="black";
-    dark2.style.color="black";
-    dark3.style.color="black";
-    h2.style.color ="black";
-    h1.style.color ="#fc0000";
+function colorBlack() {
+    dark.style.color = "black";
+    dark1.style.color = "black";
+    dark2.style.color = "black";
+    dark3.style.color = "black";
+    h2.style.color = "black";
+    h1.style.color = "#fc0000";
 }
-function backGroundBlack(){
-    nav.style.backgroundColor ="black"
-    navList.style.backgroundColor ="rgba(0, 0, 0, 0.562)";
-    line1.style.backgroundColor="white";
-    line2.style.backgroundColor="white";
-    line3.style.backgroundColor="white";
-    inicial.style.backgroundColor="#212529";
-    document.body.style.backgroundColor="#212529";
+function backGroundBlack() {
+    nav.style.backgroundColor = "black"
+    navList.style.backgroundColor = "rgba(0, 0, 0, 0.562)";
+    line1.style.backgroundColor = "white";
+    line2.style.backgroundColor = "white";
+    line3.style.backgroundColor = "white";
+    inicial.style.backgroundColor = "#212529";
+    document.body.style.backgroundColor = "#212529";
     botao.classList.remove("btn-outline-secondary");
-    botao.classList.add("btn-warning");
+    botao.classList.add("btn-light");
 }
-function backGroundWhite(){
-    nav.style.backgroundColor="white";
-    navList.style.backgroundColor="white";
-    line1.style.backgroundColor="black";
-    line2.style.backgroundColor="black";
-    line3.style.backgroundColor="black";
-    inicial.style.backgroundColor="#f8f9fa";
-    document.body.style.backgroundColor="white"
-    botao.classList.remove("btn-warning");
-    botao.classList.add("bbtn-outline-secondary");
+function backGroundWhite() {
+    nav.style.backgroundColor = "white";
+    navList.style.backgroundColor = "white";
+    line1.style.backgroundColor = "black";
+    line2.style.backgroundColor = "black";
+    line3.style.backgroundColor = "black";
+    inicial.style.backgroundColor = "#f8f9fa";
+    document.body.style.backgroundColor = "white";
+    botao.classList.remove("btn-light");
+    botao.classList.add("btn-outline-secondary")
 }
+function buttonsYellow() {
+    for (let i = 0; i < buttons.length; i++) {
 
+        buttons[i].classList.remove("btn-danger");
+        buttons[i].classList.add("btn-warning");
 
+    }
+}
+function buttonsRed() {
+    for (let i = 0; i < buttons.length; i++) {
+
+        buttons[i].classList.remove("btn-outline-warning");
+        buttons[i].classList.add("btn-danger");
+        
+    }
+}
