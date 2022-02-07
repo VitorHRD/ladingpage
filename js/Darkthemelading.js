e8d472d3a8fa8d0b1dd1a3e43aace9d4c456b2ae
VitorHRD <vrdev435@gmail.com>
2022-02-07 01:58:35 -0300

    origin

---
+++ js/Darkthemelading.js
@@ -0,0 +1,112 @@
+let h1 = document.querySelector("h1")
+let h2 = document.querySelector("h2")
+let inicial = document.getElementById("inicial");
+let img = document.getElementById("logo");
+let dark = document.getElementById("dark");
+let dark1 = document.getElementById("dark1");
+let dark2 = document.getElementById("dark2");
+let dark3 = document.getElementById("dark3");
+let nav = document.querySelector("nav");
+let navList = document.getElementById("nav-list");
+let line1 = document.getElementById("line1");
+let line2 = document.getElementById("line2");
+let line3 = document.getElementById("line3");
+let botao = document.getElementById("botao");
+let buttons = document.querySelectorAll(".buttongroup");
+let dropdown= document.querySelector(".dropdown-menu");
+
+function emojicon() {
+
+    if (emoji.style.display == "block") {
+
+        emoji.style.display = "none";
+        emoji2.style.display = "block";
+        img.src = "./imagens/Logowhite.png";
+        dropdown.classList.remove("dropdown-menu-dark");
+        colorBlack();
+        backGroundWhite();
+        buttonsRed();
+        localStorage.setItem("theme", "light");
+
+    }
+    else {
+        emoji.style.display = "block";
+        emoji2.style.display = "none";
+        img.src = "./imagens/logoblack.png";
+        dropdown.classList.add("dropdown-menu-dark");
+        colorWhite();
+        backGroundBlack();
+        buttonsYellow();
+        localStorage.setItem("theme", "dark");
+
+    }
+}
+window.onload = checkTheme();
+function checkTheme() {
+    let localStorageTheme = localStorage.getItem("theme");
+    if (localStorageTheme != null && localStorageTheme === "light") {
+        emoji.style.display = "none";
+        emoji2.style.display = "block";
+        img.src = "./imagens/Logowhite.png"
+        dropdown.classList.remove("dropdown-menu-dark");
+        colorBlack();
+        backGroundWhite();
+        buttonsRed();
+    }
+    else {
+        emoji2.style.display = "none";
+        emoji.style.display = "block";
+        img.src = "./imagens/logoblack.png"
+        dropdown.classList.add("dropdown-menu-dark");
+        colorWhite();
+        backGroundBlack();
+        buttonsYellow();
+    }
+}
+function colorWhite() {
+    dark.style.color = "white";
+    dark1.style.color = "white";
+    dark2.style.color = "white";
+    dark3.style.color = "white";
+
+}
+function colorBlack() {
+    dark.style.color = "black";
+    dark1.style.color = "black";
+    dark2.style.color = "black";
+    dark3.style.color = "black";
+}
+function backGroundBlack() {
+    nav.style.backgroundColor = "black"
+    navList.style.backgroundColor = "rgba(0, 0, 0, 0.562)";
+    line1.style.backgroundColor = "white";
+    line2.style.backgroundColor = "white";
+    line3.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "#212529";
+
+}
+function backGroundWhite() {
+    nav.style.backgroundColor = "white";
+    navList.style.backgroundColor = "white";
+    line1.style.backgroundColor = "black";
+    line2.style.backgroundColor = "black";
+    line3.style.backgroundColor = "black";
+    document.body.style.backgroundColor = "white";
+
+}
+function buttonsYellow() {
+    for (let i = 0; i < buttons.length; i++) {
+
+        buttons[i].classList.remove("btn-outline-danger");
+        buttons[i].classList.add("btn-warning");
+
+    }
+}
+function buttonsRed() {
+    for (let i = 0; i < buttons.length; i++) {
+
+        buttons[i].classList.remove("btn-warning");
+        buttons[i].classList.add("btn-outline-danger");
+
+    }
+}
